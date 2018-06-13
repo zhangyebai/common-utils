04398eb6fdbb4b8154bc00fdbf454a1c5b013fff
zhangyebai <zhangyebai@126.com>
2018-06-13 16:53:52 +0800

    packed

--- src/main/java/com/yebai/common/utils/generator/ClassUtil.java
+++ src/main/java/com/yebai/common/utils/generator/ClassUtil.java
@@ -13,21 +13,18 @@ public class ClassUtil {
 			final T t =  clazz.newInstance();
 			Field[] fields = clazz.getDeclaredFields();
 			Field.setAccessible(fields, true);
-			for(int index = 0, length = fields.length; index < length; ++index){
-				Field field = fields[index];
+			for (Field field : fields) {
 				field.setAccessible(true);
-				if(field.isAnnotationPresent(RandomString.class)){
+				if (field.isAnnotationPresent(RandomString.class)) {
 					RandomString randomString = field.getDeclaredAnnotation(RandomString.class);
 					field.set(t, RandomUtil.randomString(randomString.length()));
-				}else if(field.isAnnotationPresent(RandomInt.class)){
+				} else if (field.isAnnotationPresent(RandomInt.class)) {
 					RandomInt randomInt = field.getDeclaredAnnotation(RandomInt.class);
 					field.set(t, RandomUtil.randomInt(randomInt.range()));
 				}
 			}
 			return t;
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
+		} catch (InstantiationException | IllegalAccessException e) {
 			e.printStackTrace();
 		}
 		return null;
